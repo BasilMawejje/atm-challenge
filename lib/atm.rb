98dace1f9cfb0849bcb97c3f767d7e7446992c4b
basil <basil.mawejje@gmail.com>
2017-08-22 10:47:45 +0200

    Testing the sad path

--- lib/atm.rb
+++ lib/atm.rb
@@ -9,6 +9,8 @@ class Atm
     case
     when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date: Date.today }
+    when insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
     else
       perform_transaction(amount, account)
     end
@@ -24,5 +26,9 @@ class Atm
     @funds -= amount
     account.balance = account.balance - amount
     { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
+  def insufficient_funds_in_atm?(amount)
+    @funds < amount
   end
 end
