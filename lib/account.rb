d72ae65c18b7d478dfbfdea00270b960ab83ff46
basil <basil.mawejje@gmail.com>
2017-08-22 16:49:15 +0200

    Expiry date

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,14 @@
 class Account
-  attr_accessor :pincode
+  STANDARD_VALIDITY_YRS = 5
+
+  attr_accessor :pin_code, :exp_date
 
   def initialize
-    @pincode = 0000
+    @pincode = random_pin
+    @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  def random_pin
+    rand(1000..9999)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,23 @@
 require './lib/account.rb'
+require 'date'
 
 describe Account do
   it 'needs a pincode to access account' do
-    expect(subject.pincode).to eq 0000
+    expect(subject.pincode).to eq random_pin
+  end
+
+  #it 'check length of a number' do
+  #  number_length = Math.log10(subject.pincode).to_i + 1
+  #  expect(number_length).to eq 4
+  #end
+
+  it 'check the length of pincode' do
+    pincode = '1234'
+    expect(pincode.length).to eq 4
+  end
+
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
   end
 end
