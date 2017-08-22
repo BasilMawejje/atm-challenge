d753a976225e589d3f5e4d53edb956eec29e9e69
basil <basil.mawejje@gmail.com>
2017-08-22 17:08:19 +0200

    Account balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,12 @@
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code, :exp_date
+  attr_accessor :pin_code, :exp_date, :balance
 
   def initialize
     @pincode = random_pin
     @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    @balance = 0
   end
 
   def random_pin
