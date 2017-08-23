9154e3f04eb125285c40fa0a1fbab3b252dee36f
basil <basil.mawejje@gmail.com>
2017-08-23 13:01:29 +0200

    The Person has account attribute added

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,22 @@ class Person
   attr_accessor :name, :cash, :account
 
   def initialize(attr = {})
-    @name = Person.name
-    @cash = 0
-    @account = nil
+    if @name != nil
+      no_name
+    else
+      @name = Person.name
+      @cash = 0
+      @account = nil
+    end
+  end
+
+  def create_account
+  end
+
+  def deposit
+  end
+
+  def no_name
+    raise 'A name is required'
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,8 +2,9 @@ require './lib/person'
 require './lib/atm'
 
 describe Person do
-
-  subject { described_class.new(name: 'Thomas') }
+  # before do
+    subject { described_class.new(name: 'Basil') }
+  # end
 
   it 'is expected to have a :name on initialize' do
     expect(subject.name).not_to be nil
@@ -22,16 +23,29 @@ describe Person do
   end
 
   describe 'can create an Account' do
-    # As a Person,
-    # in order to be able to use banking services to manage my funds,
-    # i would like to be able to create a bank account
-    before { subject.create_account }
+    let(:person) { described_class.new(name: 'Basil') }
+    let(:account) {described_class.new()}
+    before { person.create_account }
     it 'of Account class ' do
-      expect(subject.account).to be_an_instance_of Account
+      expect(person.account).to be_an_instance_of Account
     end
 
     it 'with himself as an owner' do
       expect(subject.account.owner).to be subject
+    end
+  end
+
+  describe 'can manage funds if an account been created' do
+    let(:atm) { Atm.new }
+    before { subject.create_account }
+    it 'can deposit funds' do
+      expect(subject.deposit(100)).to be_truthy
+    end
+  end
+
+  describe 'can not manage funds if no account been created' do
+    it 'can\'t deposit funds' do
+      expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
     end
   end
 end
