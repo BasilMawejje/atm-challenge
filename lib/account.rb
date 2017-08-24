56b3e3a411e0f7003a2fc334a10307abedcdda24
basil <basil.mawejje@gmail.com>
2017-08-24 11:37:42 +0200

    finally all specs fully run now

--- lib/account.rb
+++ lib/account.rb
@@ -1,25 +1,29 @@
+require 'date'
+
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code, :exp_date, :balance, :account_status, :owner
+  attr_accessor :owner, :pin_code, :exp_date, :account_status, :balance
 
   def initialize(attrs = {})
-    @pincode = random_pin
+    @pin_code = rand(1000..9999)
     @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     @balance = 0
     @account_status = :active
     set_owner(attrs[:owner])
   end
 
-  def random_pin
-    rand(1000..9999)
-  end
-
-  def set_owner(obj)
-    obj == nil ?  missing_owner : @owner = obj
-  end
-
   def deactivate
     @account_status = :deactivated
+  end
+
+  private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An account owner is required'
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,43 +5,35 @@ describe Account do
   let(:person) {instance_double('Person', name: 'Basil')}
   subject { described_class.new({owner: person}) }
 
-  it 'is expected to have an owner' do
-    expect(subject.owner).to eq person
-  end
-
-  it 'is expected to raise error if no owner is set' do
-    expect { described_class.new }.to raise_error 'An Account owner is required'
-  end
-
-  it 'needs a pincode to access account' do
-    expect(subject.pincode).to eq random_pin
-  end
-
-  #it 'check length of a number' do
-  #  number_length = Math.log10(subject.pincode).to_i + 1
-  #  expect(number_length).to eq 4
-  #end
-
   it 'check the length of pincode' do
     pincode = '1234'
     expect(pincode.length).to eq 4
   end
 
-  it 'is expected to have an expiry date on initialize' do
+  it 'expires in exactly 5 years from initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
   end
 
-  it 'is expected to have a balance of 0 on initialize' do
+  it 'has a balance of 0 on initialize' do
     expect(subject.balance).to eq 0
   end
 
-  it 'is expected to have :active status on initialize' do
+  it 'has account status :active on initialize' do
     expect(subject.account_status).to eq :active
   end
 
-  it 'deactivates account using Instance method' do
+  it 'deactivates account using an instance method' do
     subject.deactivate
     expect(subject.account_status).to eq :deactivated
   end
+
+  it 'has an account owner' do
+    expect(subject.owner).to eq person
+  end
+
+  it 'raises an error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An account owner is required'
+  end
+
 end
